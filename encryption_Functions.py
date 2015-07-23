a7454c0cfb58266bcec870864bdcbc309e8cf95c
tohjustin <tohjustin@hotmail.com>
2015-07-22 22:48:43 -0700

    Done with functions, deciding whether to add a checkSum

--- encryption_Functions.py
+++ encryption_Functions.py
@@ -7,144 +7,160 @@
 
 #----------------------------------------------------------------------
 # Approach :
-# 1. Create a function that receives a string, VID, PID & Key 
+# 1. Create a function that receives a string, KeyCode composed of VID, PID & KEY 
 #    -> apply the encryption algorithm on the string (using the other param)
 #	 -> return the encrypted string
 # 2. Encryption Algorithm:
-# 	 -> Add a "checksum" at the start of the textfile to check whether
-# 		the VID & PID of the current device matches the one used for encryption
-#	 -> Will most likely add up the ASCII values of the (VID & PID) with the
-#		string characters.
-#	 -> The Key will be used to scramble the string even more xD
+# 	 -> Add a "checksum" at the start of the textfile to check whether the VID & PID of the 
+#		current device matches the one used for encryption (not sure if I want to implement this...)
+#	 -> Will most likely add up the ASCII values of the KeyCode with the input string.
 # 
-# Of course, this would restrict the program to work with only ASCII-based
-# textfiles... 
+# Of course, this would restrict the program to work with only ASCII-based textfiles... 
 #----------------------------------------------------------------------
 
-# This have to be REVERSIBLE!! >_<
-# 1. Count the string length
-# 2. Convert it into a 4-digit number
-#   - textfile needs to have no more than 9999 char per line
-#	- Map the numbers of the 4-digits into another set of character
-#	- Easy way of doing this is to take the [SHIFT] equivalent (eg. 1=!,2=@,3=#, etc.)
-#	- IF a line has 9675 chars, THEN our 4-digit code will be "(^&%"
-# 3. Our encrypted string would start with this 4-digit code
-#   - eg. XXXX-------ENCRYPTED STRING-------... (where XXXX == 4-digit code)
-# 3. 
-# XXXX = 4 digit number to indicate how many char the string originally has
-# 
-# 1234 1234 1234 1234 1234 1234 1234 1234 1234 
-# 	 VIDW PIDX VIDY PIDZ VIDW PIDX VIDY PIDZ // "WXYZ" is the 4-digit KEY that we have...
+#**********************************************************************
+# encryptThisString
+#   - encrypts an input string using the key provided & returns it
+#----------------------------------------------------------------------
+# PARAM:
+# inputString - Input string to be encrypted
+# keyCode - a string made up of the VID + PID + KEY
+#----------------------------------------------------------------------
+# RETURN:   
+# tag_value => encrypted string
+#**********************************************************************
+def encryptThisString( inputString, keyCode ):
 
-# This have to be REVERSIBLE!! >_<
-# This have to be REVERSIBLE!! >_<
-# This have to be REVERSIBLE!! >_<
-#
-# 1. Input strings are being divided into 2 classes,
-# 	a. Length <= 100 chars
-# 	b. Length > 100 chars
-#
-# a. We gotta extend these strings to meet the 100 char requirement
-#    So we append a 4 digit code at the start to identify how to encrypt it...
-#      - ">>XX" + "------encrypted_string-------...."
-#      - >> = indicates that it's a class 1 string
-#      - XX = indicates the original string length converted to it's [SHIFT] equivalent (eg. 1=!,2=@,3=#, etc.)
-#      - eg. a string length of 65 will get aa 4 digit code ">>^%" (note that 65 was converted to its shift equivalent)
-#
-# b. No extension required
-#    So we append a 4 digit code at the start to identify how to encrypt it...
-#      - "????" + "------encrypted_string-------...."
-#      - ???? = indicates that it's a class 2 string
-#
-# 2. Need to think of how to encypt the string lol...
-#
+	# Adds 4 chars @ the front of the string to identify the class of the original string
+	# Class 1 --> string that has < 100 chars
+	#		  --> 4 char format = ">>XX", where XX is the length of the original string
+	# Class 2 --> string that has >= 100 chars
+	#		  --> 4 char format = "????"
+	encryptedString = ""
+	if len(inputString) <= 10:		encryptedString = ">>0" + str(len(inputString))
+	elif len(inputString) <= 100:	encryptedString = ">>" + str(len(inputString))
+	else:							encryptedString = "????"
 
-def encryptThisString( inputString, key ):
+	# Append the string-to-be-encrypted w/ "!@#$%^&*()" until its length > 100
+	while len(inputString) < 100:
+		inputString = inputString + "!@#$%^&*()"
 
-	encryptedString = []
-	if len(inputString) <= 100:
-		encryptedString = ">>"		# A class 1 string, extend it!
-		encryptedString = encryptedString + str(len(inputString))
-		while len(inputString) < 100:
-			inputString = inputString + "!@#$%^&*()"
-	else:
-		encryptedString = "????"	# A class 2 string!!
-
-	print( "inputString = " + inputString )
+	# Forces the input strings to be in ASCII-byte format instead of unicode
+	# BYTE format required to carry out the encryption operation
 	inputString = inputString.encode('ascii', 'ignore')
-	key = key.encode('ascii', 'ignore')
+	keyCode = keyCode.encode('ascii', 'ignore')
 
-	# Scrambling Algorithm
-	j = 0							# Initialize a j-variable to rotate the chars in scrambleString
+	#****************************************************
+	# Encryption Algorithm
+	#****************************************************
+	j = 0
+	# Encrypts the whole string char-by-char
 	for i in range( len(inputString) ):
-		# Does a simple encryption by adding ASCII values of the respective chars in both strings
-	#	print( i )
-		tmp_ASCII_value = inputString[i] + key[j]
-		print(tmp_ASCII_value, end="")
-		print("=", end="")
-		print( inputString[i], end="")
-		print("+", end="")
-		print( key[j])
+		
+		# Encryption is done by adding ASCII values of the respective chars in both strings
+		tmp_ASCII_value = inputString[i] + keyCode[j]
 
+		# If the values exceed the ASCII range of 0~127, make it overflow/underflow
 		if tmp_ASCII_value < 0:
-			tmp_ASCII_value + 255
-		elif tmp_ASCII_value > 255:
-			tmp_ASCII_value - 255
+			tmp_ASCII_value = tmp_ASCII_value + 127
+		elif tmp_ASCII_value > 127:
+			tmp_ASCII_value = tmp_ASCII_value - 127
 
-		tmp_ASCII = chr(tmp_ASCII_value)
-
-		encryptedString = encryptedString + tmp_ASCII
+		# Append the encrypted char to the rest of the encryptedString
+		encryptedString = encryptedString + chr(tmp_ASCII_value)
+		
+		# Rotates the char used from keyCode on every iteration
 		j = j + 1
-		print(j)
-		if j >= len(key):	
+		if j >= len(keyCode):	
 			j = 0 
 
-	return encryptedString.encode('ascii', 'ignore')
+	# Returns the encrypted string
+	return encryptedString
 
-# def decryptThisString( inputString, key ):
+#**********************************************************************
+# decryptThisString
+#   - decrypts an input string using the key provided & returns it
+#----------------------------------------------------------------------
+# PARAM:
+# inputString - Input string to be encrypted
+# keyCode - a string made up of the VID + PID + KEY
+#----------------------------------------------------------------------
+# RETURN:   
+# tag_value => decrypted string
+#**********************************************************************
+def decryptThisString( inputString, keyCode ):
 
-# 	decryptedString = ""
-# 	decode = inputString[0:2]
+	decryptedString = ""
 
-# 	if ">>" in decode:
-# 		str_length = int( inputString[3:4] )
-# 		tmp_string = inputString[4:str_length+4]
-# 	else:
-# 		tmp_string = inputString
+	# Identifies the class of the original string by extracting the first 4 chars
+	if ">>" in inputString:
+		originalStringLength = int( inputString[2:4] )
+		tmp_string = inputString[4:originalStringLength+4]
+	else:
+		tmp_string = inputString
 
-# 	decryptedString = ""
+	# Forces the input strings to be in ASCII-byte format instead of unicode
+	# BYTE format required to carry out the decryption operation
+	tmp_string = tmp_string.encode('ascii', 'ignore')
+	keyCode = keyCode.encode('ascii', 'ignore')
 
-# 	j = 0
-# 	for i in range( len(tmp_string) ):
-# 		# Does a simple encryption by adding ASCII values of the respective chars in both strings
-# 		tmp_ASCII_value = ord(tmp_string[i]) - ord(key[j])
-# 		if tmp_ASCII_value < 0:
-# 			tmp_ASCII_value + 255
-# 		elif tmp_ASCII_value > 255:
-# 			tmp_ASCII_value - 255
+	#****************************************************
+	# Decryption Algorithm
+	#****************************************************
+	j = 0							
+	for i in range( len(tmp_string) ):
 
-# 		decryptedString = decryptedString + chr(tmp_ASCII_value))
+		# Decrypts by reversing what was done in the encryption function
+		tmp_ASCII_value = tmp_string[i] - keyCode[j]
 
-# 		j = j + 1
-# 		if j >= len(key):	
-# 			j = 0 
-			
-# 	return decryptedString
+		# If the values exceed the ASCII range of 0~127, make it overflow/underflow
+		if tmp_ASCII_value < 0:
+			tmp_ASCII_value = tmp_ASCII_value + 127
+		elif tmp_ASCII_value > 127:
+			tmp_ASCII_value = tmp_ASCII_value - 127
+
+		# Append the decrypted char to the rest of the decryptedString
+		decryptedString = decryptedString + chr(tmp_ASCII_value)
+
+		# Rotates the char used from keyCode on every iteration
+		j = j + 1
+		if j >= len(keyCode):	
+			j = 0 
+
+	# Returns the decrypted string	
+	return decryptedString
 
 #**********************************************************************
 # TESTBENCH
 #**********************************************************************
 if __name__ == '__main__':
 
-	sampleString = "ooooooooooooooooooooooooooooooooooooo"
-	encryptedString = encryptThisString(sampleString,"0000FFFF123456")
-	#decryptedString = decryptThisString(sampleString,encryptedString)
-	print( sampleString )
-	print("")
-	print("")
-	print( encryptedString.decode() )
-	# print("")
-	# print("")
-	# print( decryptedString )
+	VID = "0000"
+	PID = "1111"
+	KeyNumber = "123456"
+	sampleString = "This is just a test"
+	keyCode = VID + PID + KeyNumber 
 
-	input("Done")
+	print( "Sample String:")
+	print( sampleString )
+	print( "")
+	encryptedString = encryptThisString(sampleString,keyCode)
+	print( "Encrypted String:")
+	print( encryptedString )
+	print( "")
+	decryptedString = decryptThisString(encryptedString,keyCode)
+	print( "Decrypted String:")
+	print( decryptedString )
+	print( "")
+
+	input("<<--End of Test-->>")
+
+# #****************************************************
+# # Used for debugging the decrypt & encrypt algorithm
+# #****************************************************
+# print(tmp_ASCII_value, end="")
+# print("=", end="")
+# print( inputString[i], end="")
+# print("+", end="")
+# print( keyCode[j], end="")
+# print(" --> ", end="")
