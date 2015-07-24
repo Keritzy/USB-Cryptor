9bb22bb0a83322f54769e46246936fd97e06d7d9
tohjustin <tohjustin@hotmail.com>
2015-07-23 23:38:23 -0700

    Decided to use Tkinter as GUI framework + outline the widget classes

---
+++ classes.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+#----------------------------------------------------------------------
+# Main Objective:
+# Create classes for widget since they're going to be heavily customized to look
+# modern & non-native...
+#----------------------------------------------------------------------
+
+#----------------------------------------------------------------------
+# Approach :
+# - Decided to just stick w/ Tkinter as the GUI framework for this project >_<
+# - The UI will be really simple & straight forward to use (refer to mockup!)
+# -> InputFileWidget:
+# 		- filename entry field
+# 		- icon beside the entry field to open the fileDialog window
+# -> KeyWidget
+# 		- Customized image label "KEY"
+# 		- KEY value entry field 
+# -> ModeWidget
+# 		- Probably a slide widget to toggle betw. Encryption & Decryption ModeWidget
+# -> StartButtonWidget
+# 		- Button to execute the entire programming sequence:
+# 			1. Obtain inital list of USB VID PID 
+# 			2. Prompt user to insert USB device (via msgBox)
+# 			3. Obtain final list of USB VID PID 
+# 			4. CheckSum??
+# 			5. Begin Encryption/Decryption
+#----------------------------------------------------------------------
+class InputFileWidget:
+class KeyWidget:
+class ModeWidget:
+class StartButtonWidget
+
+
+
+
